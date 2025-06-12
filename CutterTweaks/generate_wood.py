5835132e0c4d53c29ee592c8c40c96ee1ded35e4
Garret Lowe <garret.lowe98@gmail.com>
2025-06-12 05:10:34 -0400

    Expand script for mod support

--- CutterTweaks/generate_wood.py
+++ CutterTweaks/generate_wood.py
@@ -1,141 +1,94 @@
 import json
 
-OUTPUT_DIR = './data/cptnjtk/recipe/'
+wood_map = {
+	'minecraft': {
+		'woods': ['oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove', 'cherry', 'pale_oak'],
+		'stems': ['crimson', 'warped'],
+		'blocks': ['bamboo']
+	},
+}
 
-WOOD_TYPES = ['oak', 'spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove', 'cherry', 'pale_oak']
-STEM_TYPES = ['crimson', 'warped']
-BLOCK_TYPES = ['bamboo']
-
-def write_file(from_block, to_block, count):
+def write_file(from_block, to_block, count, namespace='minecraft'):
 	new_json = {
-		"type": "minecraft:stonecutting",
-		"ingredient": f"minecraft:{from_block}",
+		"type": f"minecraft:stonecutting",
+		"ingredient": f"{namespace}:{from_block}",
 		"result": {
-			"id": f"minecraft:{to_block}",
+			"id": f"{namespace}:{to_block}",
 			"count": count
 		}
 	}
 	
+	OUTPUT_DIR = f'./{namespace}/data/cptnjtk/recipe/'
 	with open(OUTPUT_DIR + f"{to_block}_from_{from_block}_stonecutting.json", 'w') as file:
 		json.dump(new_json, file, indent=4)
 
-### PLANKS
-def planks_to_stairs():
-	for wood in WOOD_TYPES + STEM_TYPES + BLOCK_TYPES:
-		write_file(f"{wood}_planks", f"{wood}_stairs", 1)
+def generate_recipes(namespace: str, namespace_items: dict):
+	for wood in namespace_items['woods']:
+		# Planks
+		write_file(f"{wood}_planks", f"{wood}_stairs", 1, namespace)
+		write_file(f"{wood}_planks", f"{wood}_slab", 2, namespace)
+		write_file(f"{wood}_planks", f"{wood}_fence", 1, namespace)
+		write_file(f"{wood}_planks", f"{wood}_fence_gate", 1, namespace)
 
-def planks_to_slabs():
-	for wood in WOOD_TYPES + STEM_TYPES + BLOCK_TYPES:
-		write_file(f"{wood}_planks", f"{wood}_slab", 2)
+		# Wood
+		write_file(f"{wood}_wood", f"{wood}_planks", 4, namespace)
+		write_file(f"{wood}_wood", f"stripped_{wood}_wood", 1, namespace)
+		write_file(f"{wood}_wood", f"{wood}_log", 1, namespace)
 
-def planks_to_fence():
-	for wood in WOOD_TYPES + STEM_TYPES + BLOCK_TYPES:
-		write_file(f"{wood}_planks", f"{wood}_fence", 1)
+		# Stripped Wood
+		write_file(f"stripped_{wood}_wood", f"{wood}_planks", 4, namespace)
+		write_file(f"stripped_{wood}_wood", f"stripped_{wood}_log", 1, namespace)
 
-def planks_to_fence_gate():
-	for wood in WOOD_TYPES + STEM_TYPES + BLOCK_TYPES:
-		write_file(f"{wood}_planks", f"{wood}_fence_gate", 1)
+		# Logs
+		write_file(f"{wood}_log", f"{wood}_planks", 4, namespace)
+		write_file(f"{wood}_log", f"stripped_{wood}_log", 1, namespace)
+		write_file(f"{wood}_log", f"{wood}_wood", 1, namespace)
 
+		# Stripped Logs
+		write_file(f"stripped_{wood}_log", f"{wood}_planks", 4, namespace)
+		write_file(f"stripped_{wood}_log", f"stripped_{wood}_wood", 1, namespace)
 
-### WOOD
-def wood_to_planks():
-	for wood in WOOD_TYPES:
-		write_file(f"{wood}_wood", f"{wood}_planks", 4)
-			
-	for stem in STEM_TYPES:
-		write_file(f"{stem}_hyphae", f"{stem}_planks", 4)
-		
-def wood_to_stripped_wood():
-	for wood in WOOD_TYPES:
-		write_file(f"{wood}_wood", f"stripped_{wood}_wood", 1)
-			
-	for stem in STEM_TYPES:
-		write_file(f"{stem}_hyphae", f"stripped_{stem}_hyphae", 1)
+	for stem in namespace_items['stems']:
+		# Planks
+		write_file(f"{stem}_planks", f"{stem}_stairs", 1, namespace)
+		write_file(f"{stem}_planks", f"{stem}_slab", 2, namespace)
+		write_file(f"{stem}_planks", f"{stem}_fence", 1, namespace)
+		write_file(f"{stem}_planks", f"{stem}_fence_gate", 1, namespace)
 
-def wood_to_log():
-	for wood in WOOD_TYPES:
-		write_file(f"{wood}_wood", f"{wood}_log", 1)
-			
-	for stem in STEM_TYPES:
-		write_file(f"{stem}_hyphae", f"{stem}_stem", 1)
-	
-### LOGS
-def log_to_planks():
-	for wood in WOOD_TYPES:
-		write_file(f"{wood}_log", f"{wood}_planks", 4)
-			
-	for stem in STEM_TYPES:
-		write_file(f"{stem}_stem", f"{stem}_planks", 4)
-        
-	for block in BLOCK_TYPES:
-		write_file(f"{block}_block", f"{block}_planks", 2)
-	
-def log_to_stripped_log():
-	for wood in WOOD_TYPES:
-		write_file(f"{wood}_log", f"stripped_{wood}_log", 1)
-			
-	for stem in STEM_TYPES:
-		write_file(f"{stem}_stem", f"stripped_{stem}_stem", 1)
-        
-	for block in BLOCK_TYPES:
-		write_file(f"{block}_block", f"stripped_{block}_block", 1)
-	
-def log_to_wood():
-	for wood in WOOD_TYPES:
-		write_file(f"{wood}_log", f"{wood}_wood", 1)
-			
-	for stem in STEM_TYPES:
-		write_file(f"{stem}_stem", f"{stem}_hyphae", 1)
+		# Hyphae
+		write_file(f"{stem}_hyphae", f"{stem}_planks", 4, namespace)
+		write_file(f"{stem}_hyphae", f"stripped_{stem}_hyphae", 1, namespace)
+		write_file(f"{stem}_hyphae", f"{stem}_stem", 1, namespace)
 
+		# Stripped Hyphae
+		write_file(f"stripped_{stem}_hyphae", f"{stem}_planks", 4, namespace)
+		write_file(f"stripped_{stem}_hyphae", f"stripped_{stem}_stem", 1, namespace)
 
-### STRIPPED
-def stripped_wood_to_planks():
-	for wood in WOOD_TYPES:
-		write_file(f"stripped_{wood}_wood", f"{wood}_planks", 4)
-			
-	for stem in STEM_TYPES:
-		write_file(f"stripped_{stem}_hyphae", f"{stem}_planks", 4)
-	
-def stripped_wood_to_stripped_log():
-	for wood in WOOD_TYPES:
-		write_file(f"stripped_{wood}_wood", f"stripped_{wood}_log", 1)
-			
-	for stem in STEM_TYPES:
-		write_file(f"stripped_{stem}_hyphae", f"stripped_{stem}_stem", 1)
-		
-def stripped_log_to_planks():
-	for wood in WOOD_TYPES:
-		write_file(f"stripped_{wood}_log", f"{wood}_planks", 4)
-			
-	for stem in STEM_TYPES:
-		write_file(f"stripped_{stem}_stem", f"{stem}_planks", 4)
-			
-	for block in BLOCK_TYPES:
-		write_file(f"stripped_{block}_block", f"{block}_planks", 2)
-	
-def stripped_log_to_stripped_wood():
-	for wood in WOOD_TYPES:
-		write_file(f"stripped_{wood}_log", f"stripped_{wood}_wood", 1)
-			
-	for stem in STEM_TYPES:
-		write_file(f"stripped_{stem}_stem", f"stripped_{stem}_hyphae", 1)
+		# Stems
+		write_file(f"{stem}_stem", f"{stem}_planks", 4, namespace)
+		write_file(f"{stem}_stem", f"stripped_{stem}_stem", 1, namespace)
+		write_file(f"{stem}_stem", f"{stem}_hyphae", 1, namespace)
 
+		# Stripped Stems
+		write_file(f"stripped_{stem}_stem", f"{stem}_planks", 4, namespace)
+		write_file(f"stripped_{stem}_stem", f"stripped_{stem}_hyphae", 1, namespace)
 
-### MAIN
-planks_to_stairs()
-planks_to_slabs()
-planks_to_fence()
-planks_to_fence_gate()
+	for block in namespace_items['blocks']:
+		# Planks
+		write_file(f"{block}_planks", f"{block}_stairs", 1, namespace)
+		write_file(f"{block}_planks", f"{block}_slab", 2, namespace)
+		write_file(f"{block}_planks", f"{block}_fence", 1, namespace)
+		write_file(f"{block}_planks", f"{block}_fence_gate", 1, namespace)
 
-wood_to_planks()
-wood_to_stripped_wood()
-wood_to_log()
+		# Blocks
+		write_file(f"{block}_block", f"{block}_planks", 2, namespace)
+		write_file(f"{block}_block", f"stripped_{block}_block", 1, namespace)
 
-log_to_planks()
-log_to_stripped_log()
-log_to_wood()
+		# Stripped Blocks
+		write_file(f"stripped_{block}_block", f"{block}_planks", 2, namespace)
 
-stripped_wood_to_planks()
-stripped_wood_to_stripped_log()
-stripped_log_to_planks()
-stripped_log_to_stripped_wood()
+def main():
+	for namespace in wood_map:
+		generate_recipes(namespace, wood_map[namespace])
+
+main()
